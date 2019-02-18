6deb126c04ea4a0b2da9e857f010824e45e9a41a
seanmcquaid <sean.mcquaid2@verizon.net>
2019-02-17 21:29:27 -0500

    set up api call

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import '../App.css';
+import config from "../config";
+import SearchBar from "./SearchBar";
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      searchResults : []
+    }
+    this.weatherSearch = this.weatherSearch.bind(this);
+  }
+
+  weatherSearch(event){
+    event.preventDefault();
+    console.log("search started")
+    const zipCode = document.getElementById("zipCode").value;
+    console.log(zipCode)
+    console.log(config)
+    const searchUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&APPID=${config.apiKey}&units=imperial`;
+    console.log(searchUrl)
+    fetch(searchUrl)
+    .then((response)=>{
+      return (response.json())
+    }).then((myJson)=>{
+      console.log(myJson)
+      this.setState({
+        searchResults: myJson
+      })
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Weather App Practice</h1>
+        <SearchBar searchWeatherFunction={this.weatherSearch}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+class SearchBar extends Component {
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <form onSubmit={this.props.searchWeatherFunction}>
+                <input id="zipCode" type="text" placeholder="Type in Your Zip Code Here"/>
+                <button type="submit">Submit</button>
+            </form>
+        )
+    }
+}
+
+export default SearchBar;
